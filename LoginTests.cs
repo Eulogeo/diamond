94a599207253b85d755d78cb09ff7000ff0d5410
Folowosele Olaoluwa <folowoseleolaoluwa@gmail.com>
2019-12-10 14:41:21 +0000

    first commit

---
+++ LoginTests.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    [TestFixture]
+    public class LoginTests
+    {
+        
+        public IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver(); //launches browser
+            driver.Manage().Window.Maximize(); //Maximizes window
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+        }
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+        [Test]
+        public void VerifyInvalidLoginWithIncorrectPassword()
+        {
+            //Open Login page
+            driver.Url = "http://the-internet.herokuapp.com/login";
+            Console.WriteLine("Opening " + driver.Url);
+
+            //Enter corect Username and wrong Password
+            driver.FindElement(By.Id("username")).SendKeys("tomsmith");
+            driver.FindElement(By.Id("password")).SendKeys("tomsmith");
+            Console.WriteLine("Enter Username and password");
+
+            driver.FindElement(By.XPath("//i[@class='fa fa-2x fa-sign-in']")).Click();
+            Assert.IsTrue(driver.PageSource.Contains("Your password is invalid!"));
+
+        }
+        [Test]
+        public void VerifyInvalidLoginWithIncorrectUsername()
+        {
+            //Open Login page
+            driver.Url = "http://the-internet.herokuapp.com/login";
+            Console.WriteLine("Opening " + driver.Url);
+
+
+            //Enter wrong Username and correct Password
+            driver.FindElement(By.Id("username")).SendKeys("smith");
+            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
+            Console.WriteLine("Enter Username and password");
+
+            driver.FindElement(By.XPath("//i[@class='fa fa-2x fa-sign-in']")).Click();
+            Assert.IsTrue(driver.PageSource.Contains("Your username is invalid!"));
+        }
+        [Test]
+        public void VerifyValidlogin()
+        {
+            //Open Login page
+            driver.Url = "http://the-internet.herokuapp.com/login";
+            Console.WriteLine("Opening " + driver.Url);
+
+            //Enter corect Username and correct Password
+            driver.FindElement(By.Id("username")).SendKeys("tomsmith");
+            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
+            Console.WriteLine("Enter Username and password");
+
+            driver.FindElement(By.XPath("//i[@class='fa fa-2x fa-sign-in']")).Click();
+            Assert.IsTrue(driver.PageSource.Contains("Secure Area"));
+            driver.FindElement(By.XPath("//a[@href='/logout']"));
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    
+    [TestFixture]
+    public class Answer_Digital_test
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine("Main method");
+        }
+      
+        public IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver(); //launches browser
+            driver.Manage().Window.Maximize(); //Maximizes window
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+        }
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+        [Test]
+        public void VerifyInfiniteScroll()
+        {
+            //Open page
+            driver.Url = "http://the-internet.herokuapp.com/infinite_scroll";
+            Console.WriteLine("Opening " + driver.Url);
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("window.scrollBy(0,1536)", "");
+            Assert.IsTrue(driver.PageSource.Contains("Infinite Scroll"));
+
+        }
+        [Test]
+        public void VerifyKeyPresses()
+        {
+            //Open page
+            driver.Url = "http://the-internet.herokuapp.com/key_presses";
+            Console.WriteLine("Opening " + driver.Url);
+
+            driver.FindElement(By.Id("target")).SendKeys(Keys.Tab);
+            Assert.IsTrue(driver.PageSource.Contains("TAB"));
+            driver.FindElement(By.Id("target")).SendKeys(Keys.Enter);
+            Assert.IsTrue(driver.PageSource.Contains("ENTER"));
+            driver.FindElement(By.Id("target")).SendKeys(Keys.Escape);
+            Assert.IsTrue(driver.PageSource.Contains("ESCAPE"));
+            driver.FindElement(By.Id("target")).SendKeys(Keys.Space);
+            Assert.IsTrue(driver.PageSource.Contains("SPACE"));
+
+        }
+
+    }
+
+}
